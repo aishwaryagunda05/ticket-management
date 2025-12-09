33433407cedd4747b23b33b4a3be0a226625fcc9
Aishwarya Gunda <aishwaryagunda05@gmail.com>
2025-12-09 12:09:43 +0530

    commited

---
+++ TicketApi/Controllers/TicketsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketApi.models;
+
+
+namespace TicketApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TicketsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Ticket>> GetTickets()
+        {
+            var tickets = new List<Ticket>
+            {
+                new Ticket
+                {
+                    Id = 1,
+                    ShortDescription = "Login issue",
+                    Description = "User unable to login with valid credentials.",
+                    CreatedDate = new DateTime(2025, 6, 10),
+                    Severity = "High",
+                    TargetDate = new DateTime(2025, 6, 15),
+                    Status = "Open"
+                },
+                new Ticket
+                {
+                    Id = 2,
+                    ShortDescription = "Page not loading",
+                    Description = "Dashboard page is taking too long to load.",
+                    CreatedDate = new DateTime(2025, 6, 11),
+                    Severity = "Medium",
+                    TargetDate = new DateTime(2025, 6, 20),
+                    Status = "Closed"
+                }
+            };
+
+            return Ok(tickets);
+        }
+    }
+}
